d6f3878253aed82ca67abe10e76890895fd1dc57
Evan Vogts <evanjvogts@gmail.com>
2019-12-03 07:48:09 -0600

    test

--- test/movies-endpoints.spec.js
+++ test/movies-endpoints.spec.js
@@ -1,9 +1,26 @@
+const knex = require('knex');
 const app = require('../src/app');
 
-describe('App', () => {
-  it('GET / responds with 200 containing "Hello, world!"', () => {
-    return supertest(app)
-      .get('/')
-      .expect(200, 'Hello, boilerplate!');
+describe('Movies Endpoints', function() {
+  let db;
+
+  before('make knex instance', () => {
+    db = knex({
+      client: 'pg',
+      connection: process.env.TEST_DB_URL,
+    });
+    app.set('db', db);
+  });
+
+  after('disconnect from db', () => db.destroy());
+
+  describe('GET /api/movies', () => {
+    context('Given no movies', () => {
+      it('responds with 200 and an empty list', () => {
+        return supertest(app)
+          .get('/api/movies')
+          .expect(200, []);
+      });
+    });
   });
 });
